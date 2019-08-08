3aa224872904d665fc36693f91e9900a614e5c5f
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2019-08-08 22:56:49 +0100

    Updated CORS

--- app/Http/Middleware/AuthenticateOnce.php
+++ app/Http/Middleware/AuthenticateOnce.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Middleware;
 
+use Firebase\JWT\ExpiredException;
+use Firebase\JWT\JWT;
+use Firebase\JWT\SignatureInvalidException;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Closure;
+use Illuminate\Support\Facades\Cookie;
 
 class AuthenticateOnce
 {
@@ -17,6 +21,29 @@ class AuthenticateOnce
      */
     public function handle(Request $request, Closure $next)
     {
-
+        if (Cookie::has('jwt')) {
+            $jwt = $request->cookie('jwt');
+            try {
+                $decodedJwt = JWT::decode($jwt, env("APP_KEY"), ['HS256']);
+                $decodedArray = [
+                    'user_id' => $decodedJwt->user_id,
+                    'exp' => $decodedJwt->exp,
+                ];
+                if (time() < $decodedArray['exp'])
+                    if (Auth::loginUsingId($decodedArray['user_id']))
+                        return $next($request);
+                    else
+                        return response(["isSessionValid" => "false"]);
+                else
+                    return response(["isSessionValid" => "false"]);
+            } catch (ExpiredException $e) {
+                return response(["isSessionValid" => "false"]);
+            } catch (SignatureInvalidException $e) {
+                return response(["isSessionValid" => "false"]);
+            } catch (\Exception $e) {
+                return response(["isSessionValid" => "false"]);
+            }
+        } else
+            return response(["isSessionValid" => "false"]);
     }
 }
