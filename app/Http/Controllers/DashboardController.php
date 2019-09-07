1c6f230d950974876cf67c462eacf69de3cbcf62
Eric McWinNEr <mcwinnerwebdeveloper@gmail.com>
2019-09-07 10:51:36 +0100

    Updated Dashboard init to show officer-based stats

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Candidate;
+use App\OfficerRegister;
 use App\Party;
 use App\User;
+use App\Utils\UserHelper;
 use App\Utils\Utility;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -34,19 +36,36 @@ class DashboardController extends Controller
      */
     public function initializeHomePage(Request $request)
     {
+        $user = $request->user();
+        $officerInfo = null;
+        $officialInfo = null;
         $electionInfo = app(ElectionController::class)->getCurrentElectionMinimalInfo();
         $voterCount = User::count();
         $partiesCount = Party::count();
         $candidatesCount = Candidate::count();
         $pollingOfficersCount = User::whereJsonContains('roles', 'officer')->count();
-        $voterCreatedLast =
-            Utility::dateStringParser(User::latest('created_at')->first()->created_at["created_at_default"]);
-        $partyCreatedLast = Utility::dateStringParser(Party::latest('created_at')->first()->created_at);
-        $candidateCreatedLast =
-            $candidatesCount > 0 ? Utility::dateStringParser(Candidate::latest('created_at')->first()->created_at) : "N/A";
-        $officerCreatedLast =
-            $pollingOfficersCount > 0 ? Utility::dateStringParser(User::whereJsonContains('roles', 'officer')->latest('created_at')
+        $voterCreatedLast = Utility::dateStringParser(User::latest('created_at')
+                                                          ->first()->created_at["created_at_default"]);
+        $partyCreatedLast =
+            Utility::dateStringParser(Party::latest('created_at')->first()->created_at);
+        $candidateCreatedLast = $candidatesCount > 0 ?
+            Utility::dateStringParser(Candidate::latest('created_at')->first()->created_at) : "N/A";
+        $officerCreatedLast = $pollingOfficersCount > 0 ?
+            Utility::dateStringParser(User::whereJsonContains('roles', 'officer')
+                                          ->latest('created_at')
                                           ->first()->created_at["created_at_default"]) : "N/A";
+        if (UserHelper::isOfficer($user))
+        {
+            $totalVotersRegistered = OfficerRegister::where('officer_id', $user->id)->count();
+            $totalVotersRegisteredToday = OfficerRegister::where('officer_id', $user->id)
+                                                         ->whereDate('created_at', Carbon::today("UTC")
+                                                                                         ->format('Y-m-d'))
+                                                         ->count();
+            $officerInfo = [
+                "total_voters_registered"       => $totalVotersRegistered,
+                "total_voters_registered_today" => $totalVotersRegisteredToday,
+            ];
+        }
 
         return response([
             "isSessionValid" => "true",
@@ -66,7 +85,8 @@ class DashboardController extends Controller
             "officers"       => [
                 "count"       => $pollingOfficersCount,
                 "lastCreated" => $officerCreatedLast,
-            ]
+            ],
+            "officer_info"   => $officerInfo
         ]);
     }
 }
